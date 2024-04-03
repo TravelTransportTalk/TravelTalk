527699f8e660b83f3228084637f5e1803c947692
EmirVildanov <reddog201030@gmail.com>
2024-04-03 18:16:25 +0300

    Added changes

--- webui/constants.py
+++ webui/constants.py
@@ -1,9 +1,11 @@
 APP_NAME = "Еду поболтать"
 APP_SLOGAN = "Найти собседника в поездку — проще простого"
+
+# TODO: use constants instead of hardcoded values in Streamlit app.
 CREATE_TRIP = "Создать поездку"
 FIND_INTERLOCUTOR = "Найти собеседника"
-
 
 PAGE_MAIN = "entry.py"
 PAGE_EXISTING_TRIPS = "pages/existing_trips_filtration.py"
 PAGE_CREATE_NEW_TRIP = "pages/new_trip_registration.py"
+PAGE_SPECIFIC_TRIP = "pages/specific_trip.py"
